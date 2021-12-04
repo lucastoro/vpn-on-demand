56738b881d063011c9a2ab002c6f3d7a734a99d1
Luca Castoro <13522333+lucastoro@users.noreply.github.com>
2021-12-04 02:19:50 +0000

    Parametrize run_client.sh

--- run_client.sh
+++ run_client.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+test -n "$1" || {
+    echo "You have to specify an hostname/ip"
+    exit 1
+}
+
 HERE=$(dirname $(readlink -e $BASH_SOURCE))
 
-ssh -p 2222 -D 8888 -N -i $HERE/id_rsa proxy@localhost
+test -r $HERE/id_rsa || {
+    echo "Cannot access the private key file"
+    exit 1
+}
+
+ssh -p 2222 -D 8888 -N -i $HERE/id_rsa proxy@$1
